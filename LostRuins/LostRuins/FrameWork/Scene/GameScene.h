a6d11c3be3ca93b642b9fcec2a897ca4114cad31
Jeonghoon625 <Dr.kim1997@gmail.com>
2022-05-08 02:26:42 +0900

    Add UI View and Game Scene

---
+++ LostRuins/LostRuins/FrameWork/Scene/GameScene.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+#include "Scene.h"
+#include "../Mgr/TextureHolder.h"
+#include "../Object/Player/Player.h"
+#include "../Object/TestBlock/TestBlock.h"
+#include "../Object/Monster/Monster.h"
+#include "../Object/Monster/ZombieCrawler.h"
+#include "../Object/Monster/ZombieWalker.h"
+#include "TestSceneUI/TestSceneUi.h"
+
+class GameScene : public Scene
+{
+	SceneManager* sceneMgr;
+	Vector2i resolution;
+
+	Player player;
+	IntRect gameMap;
+
+	Texture textureHeroine;
+
+	Sprite heroineAction;
+
+	vector <TestBlock*> blocks;
+
+	Sprite testSprite;
+	Sprite testWeapon;
+
+	// test
+	ZombieWalker zombieWalker;
+	TestUi testUI;
+
+public:
+	virtual void Init(SceneManager* sceneManager);
+	virtual void Update(float dt, Time playTime, RenderWindow* window, View* mainView);
+	virtual void Draw(RenderWindow* window, View* mainView);
+	virtual ~GameScene();
+
+	void CreateBlock();
+};
+
